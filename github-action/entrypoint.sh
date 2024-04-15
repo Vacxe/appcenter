40ad3c41192af89eab63749f853bf94e60f5098f
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-04-15 12:00:15 +1000

    fix

--- github-action/entrypoint.sh
+++ github-action/entrypoint.sh
@@ -1,14 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
 set -e
-
-function checkParameter() {
-if [ -z "$2" ]
-then
-  echo "$1 is required, but not provided"
-  exit 1
-fi
-}
 
 APPCENTER_TOKEN="${1}"
 TEMPLATE="${2}"
@@ -23,24 +15,16 @@ PARAMETERS="${10}"
 
 echo "AppCenter version:"
 appcenter --version
-
-checkParameter "AppCenter Access Token" "$APPCENTER_TOKEN"
 export APPCENTER_ACCESS_TOKEN="$APPCENTER_TOKEN"
-
 echo "Template: $TEMPLATE"
 
 case $TEMPLATE in
 
   "custom-script")
-    checkParameter "Custom script" "$CUSTOM_SCRIPT"
     eval "$CUSTOM_SCRIPT"
     ;;
 
   "distribute-release")
-    checkParameter "File path" "$FILE"
-    checkParameter "App name" "$APP"
-    checkParameter "Group name" "$GROUP"
-
     sh /templates/distribute-release.sh "$FILE" "$APP" "$GROUP" "$RELEASE_NOTES" "$OUTPUT" "$BUILD_VERSION" "$PARAMETERS"
     ;;
 
